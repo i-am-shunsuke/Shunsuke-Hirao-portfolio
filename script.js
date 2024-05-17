2e5d4ddf60db524f0c5eb73aedd621f3c02b010c
Shunsuke-Hirao <167667313+Shunsuke-Hirao@users.noreply.github.com>
2024-05-17 06:11:34 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let introduction=document.querySelector("#introduction");
+let education=document.querySelector("#education");
+let skill=document.querySelector("#skill");
+let experience=document.querySelector("#experience");
+let work=document.querySelector("#work");
+
+let options={
+    rootMargin:"10px",
+    threshold:0.2,
+};
+let setItemActive=(entries)=>{
+    console.log(entries);
+    entries.map((entry)=>{
+        entry.isIntersecting?entry.target.classList.add("active"):null;
+    });
+};
+
+let observer = new IntersectionObserver(setItemActive, options);
+
+observer.observe(introduction);
+observer.observe(education);
+observer.observe(skill);
+observer.observe(experience);
+observer.observe(work);
